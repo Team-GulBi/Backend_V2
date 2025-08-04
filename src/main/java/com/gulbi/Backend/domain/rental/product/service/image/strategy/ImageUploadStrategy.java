f3719e64896c2e5634cbf67af6c53de378d431d6
woosungking <abuga991008@gmail.com>
2025-08-04 22:32:15 +0900

    refactor: 상품 이미지 업데이트 전략(이미지 추가)

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/strategy/ImageUploadStrategy.java
@@ -0,0 +1,40 @@
+package com.gulbi.Backend.domain.rental.product.service.image.strategy;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.request.register.NewProductImageRequest;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageUpdateCommand;
+import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.service.image.ImageCrudService;
+import com.gulbi.Backend.domain.rental.product.service.product.crud.ProductCrudService;
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrlCollection;
+@Component
+public class ImageUploadStrategy extends AbstractImageUpdateStrategy{
+	private final ImageCrudService imageCrudService;
+	public ImageUploadStrategy(ProductCrudService productCrudService, ImageCrudService imageCrudService) {
+		super(productCrudService);
+		this.imageCrudService = imageCrudService;
+	}
+
+	@Override
+	public boolean canUpdate(ProductImageUpdateCommand command) {
+		return Optional.ofNullable(command.getToBeAddedImages()).isPresent();
+	}
+
+	@Override
+	public void update(ProductImageUpdateCommand command) {
+		Long productId = command.getProductId();
+		Product product = resolveProduct(productId);
+		ImageUrlCollection imageUrlCollection = uploadImagesToS3(command.getToBeAddedImages());
+		updateNewImage(imageUrlCollection, product);
+	}
+
+	private ImageUrlCollection uploadImagesToS3(NewProductImageRequest request){
+		return imageCrudService.uploadImagesToS3(request.getProductImageCollection());
+	}
+	private void updateNewImage(ImageUrlCollection uploadedImages, Product product){
+		imageCrudService.registerImagesWithProduct(uploadedImages, product);
+	}
+}
