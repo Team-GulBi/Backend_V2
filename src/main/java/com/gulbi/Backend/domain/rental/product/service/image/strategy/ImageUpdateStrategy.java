c9556198996292525579511701d1a83404e6a667
woosungking <abuga991008@gmail.com>
2025-08-04 22:32:05 +0900

    refactor: 상품 이미지 업데이트 전략

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/strategy/ImageUpdateStrategy.java
@@ -0,0 +1,12 @@
+package com.gulbi.Backend.domain.rental.product.service.image.strategy;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductContentUpdateCommand;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageUpdateCommand;
+
+public interface ImageUpdateStrategy {
+	// 해당 요청이 내가 처리 할 전략인지 판별하는 메서드
+	boolean canUpdate(ProductImageUpdateCommand command);
+	// 해당 요청이 내가 처리 할 수 있는 전략이라면 처리(이미지 추가, 메인이미지 변경(파일), 메인이미지 변경(url), 이미지삭제)
+	void update(ProductImageUpdateCommand command);
+
+}
