cf0a26a2f8515d5ab8b4f3835b72f2bc4d4241c9
woosungking <abuga991008@gmail.com>
2025-08-04 22:33:34 +0900

    refactor: 상품 이미지 업데이트 전략 패턴 도입에 따른

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageServiceImpl.java
@@ -1,11 +1,12 @@
 package com.gulbi.Backend.domain.rental.product.service.image;
 
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductExistingMainImageUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
+import java.util.List;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageUpdateCommand;
 import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductMainImageUpdateDto;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.service.image.strategy.ImageUpdateStrategy;
 import com.gulbi.Backend.domain.rental.product.service.product.crud.ProductCrudService;
-import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
 import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrlCollection;
 import com.gulbi.Backend.domain.rental.product.vo.image.ProductImageCollection;
 import lombok.RequiredArgsConstructor;
@@ -18,66 +19,11 @@ import org.springframework.stereotype.Service;
 public class ImageServiceImpl implements ImageService {
     private final ImageCrudService imageCrudService;
     private final ProductCrudService productCrudService;
-
+    private final List<ImageUpdateStrategy> imageUpdateStrategies;
     @Override
-    public void updateProductImages(ProductImageInfoUpdateDto productImageInfoUpdateDto) {
-
-        Long productId = productImageInfoUpdateDto.getProductId();
-        Product product = resolveProduct(productId);
-
-        if(productImageInfoUpdateDto.getToBeAddedImages() !=null) {
-            handleAddedImages(productImageInfoUpdateDto, product);
-        }
-        if(productImageInfoUpdateDto.getToBeUpdatedMainImageFile() !=null) {
-            imageCrudService.clearMainImageFlags(product);
-            handleUpdatedMainImageFile(productImageInfoUpdateDto, product);
-        }
-        if(productImageInfoUpdateDto.getToBeUpdatedMainImageWithUrl() !=null) {
-            imageCrudService.clearMainImageFlags(product);
-            ProductMainImageUpdateDto productMainImageUpdateDto = ProductMainImageUpdateDto.of(productId, productImageInfoUpdateDto.getToBeUpdatedMainImageWithUrl().getMainImageUrl());
-            handleUpdatedMainImageWithUrl(productMainImageUpdateDto);
-        }
-        if(productImageInfoUpdateDto.getToBeDeletedImages() !=null){
-            handleDeletedImages(productImageInfoUpdateDto); //지우려는게 메인이미지 일때 예외처리 해주긴해야함.. todo..
-        }
+    public void updateProductImages(ProductImageUpdateCommand command) {
+        imageUpdateStrategies.stream()
+            .filter(strategy->strategy.canUpdate(command))
+            .forEach(strategy->strategy.update(command));
     }
-
-    private void handleAddedImages(ProductImageInfoUpdateDto dto, Product product) {
-            ImageUrlCollection imageUrlCollection = uploadImagesToS3(dto.getToBeAddedImages().getProductImageCollection());
-            updateNewImage(imageUrlCollection, product);
-    }
-
-    private void handleUpdatedMainImageFile(ProductImageInfoUpdateDto dto, Product product) {
-
-            ImageUrlCollection imageUrlCollection = uploadImagesToS3(dto.getToBeUpdatedMainImageFile().getProductImageCollection());
-            imageCrudService.saveMainImage(imageUrlCollection.getMainImageUrl(), product);
-
-            ProductMainImageUpdateDto updateDto = ProductMainImageUpdateDto.of(dto.getProductId(), imageUrlCollection.getMainImageUrl());
-            productCrudService.updateProductMainImage(updateDto);
-
-    }
-
-    private void handleUpdatedMainImageWithUrl(ProductMainImageUpdateDto dto) {
-        imageCrudService.updateMainImageFlags(dto);
-        productCrudService.updateProductMainImage(dto);
-    }
-
-    private void handleDeletedImages(ProductImageInfoUpdateDto dto) {
-        if (!dto.getToBeDeletedImages().getImagesId().isEmpty()) {
-            imageCrudService.deleteImages(dto.getToBeDeletedImages());
-        }
-    }
-
-    private ImageUrlCollection uploadImagesToS3(ProductImageCollection productImageCollection) {
-        return imageCrudService.uploadImagesToS3(productImageCollection);
-    }
-
-    private Product resolveProduct(Long productId) {
-        return productCrudService.getProductById(productId);
-    }
-
-    private void updateNewImage(ImageUrlCollection imageUrlCollection, Product product) {
-        imageCrudService.registerImagesWithProduct(imageUrlCollection, product);
-    }
-
 }
