75821daec28ffcb71a863d6deba0bfdc86957f39
woosungking <abuga991008@gmail.com>
2025-08-05 15:45:08 +0900

    refactor: Swagger 문서화 추가

--- src/main/java/com/gulbi/Backend/domain/rental/application/dto/ApplicationCreateRequest.java
+++ src/main/java/com/gulbi/Backend/domain/rental/application/dto/ApplicationCreateRequest.java
@@ -3,8 +3,19 @@ package com.gulbi.Backend.domain.rental.application.dto;
 import java.time.LocalDate;
 import lombok.Getter;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import java.time.LocalDateTime;
+
 @Getter
 public class ApplicationCreateRequest {
-    private LocalDate startDate;
-    private LocalDate endDate;
+
+    @Schema(description = "대여 시작일 (시간까지 포함, 분/초는 00)", example = "2025-08-10T14:00:00")
+    private LocalDateTime startDate;
+
+    @Schema(description = "대여 종료일 (시간까지 포함, 분/초는 00)", example = "2025-08-15T18:00:00")
+    private LocalDateTime endDate;
 }
