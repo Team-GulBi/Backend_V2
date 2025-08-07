d004790b0b31f74699e06c7755dc729641c0c834
woosungking <abuga991008@gmail.com>
2025-08-07 21:51:57 +0900

    refactor: 회원가입 컨트롤러 멀티파트 요청 받도록

--- src/main/java/com/gulbi/Backend/domain/user/controller/AuthController.java
+++ src/main/java/com/gulbi/Backend/domain/user/controller/AuthController.java
@@ -1,14 +1,18 @@
 package com.gulbi.Backend.domain.user.controller;
 
 
-import com.gulbi.Backend.domain.user.dto.LoginRequestDto;
-import com.gulbi.Backend.domain.user.dto.RegisterRequestDto;
+import com.gulbi.Backend.domain.user.dto.LoginRequest;
+import com.gulbi.Backend.domain.user.dto.RegisterCommand;
+import com.gulbi.Backend.domain.user.dto.RegisterRequest;
 import com.gulbi.Backend.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -18,14 +22,16 @@ public class AuthController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<String> register(@RequestBody RegisterRequestDto request) {
-        userService.register(request);
+    @PostMapping(value = "/signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> register(@RequestPart RegisterRequest request,
+        @RequestPart("signature")MultipartFile signature) throws IOException {
+        RegisterCommand command = new RegisterCommand(signature,request);
+        userService.register(command);
         return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDto request) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest request) {
         Map<String, String> response = userService.login(request);
         return ResponseEntity.ok(response);
     }
