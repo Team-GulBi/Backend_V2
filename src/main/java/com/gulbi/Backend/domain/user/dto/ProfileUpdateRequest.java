4635030f63d27a5bdef68e457639e8c6d5f8ce49
woosungking <abuga991008@gmail.com>
2025-08-07 22:01:29 +0900

    refactor:

---
+++ src/main/java/com/gulbi/Backend/domain/user/dto/ProfileUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.gulbi.Backend.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProfileUpdateRequest {
+    private String phone; // 전화번호
+}
