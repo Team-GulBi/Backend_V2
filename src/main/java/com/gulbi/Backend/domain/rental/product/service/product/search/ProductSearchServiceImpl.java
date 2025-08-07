47c0a2761aba300b90eaa27dca2d19df95e957ff
woosungking <abuga991008@gmail.com>
2025-08-07 21:59:20 +0900

    refactor: profile 일부 필드 제거

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/search/ProductSearchServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/search/ProductSearchServiceImpl.java
@@ -6,21 +6,15 @@ import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductSearchRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.response.ProductDetailResponseDto;
-import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.exception.ProductException;
-import com.gulbi.Backend.domain.rental.product.repository.ProductRepository;
 import com.gulbi.Backend.domain.rental.product.service.image.ImageCrudService;
 import com.gulbi.Backend.domain.rental.product.service.product.crud.ProductCrudService;
 import com.gulbi.Backend.domain.rental.product.service.product.logging.ProductLogHandler;
 import com.gulbi.Backend.domain.rental.product.service.product.search.strategy.search.ProductSearchStrategy;
-import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
 import com.gulbi.Backend.domain.rental.review.dto.ReviewWithAvgProjection;
 import com.gulbi.Backend.domain.rental.review.service.ReviewService;
-import com.gulbi.Backend.domain.user.dto.ProfileResponseDto;
-import com.gulbi.Backend.domain.user.entity.Profile;
-import com.gulbi.Backend.domain.user.entity.User;
+import com.gulbi.Backend.domain.user.dto.ProfileResponse;
 import com.gulbi.Backend.domain.user.service.ProfileService;
-import com.gulbi.Backend.domain.user.service.UserService;
 import com.gulbi.Backend.global.error.ExceptionMetaData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -69,9 +63,9 @@ public class ProductSearchServiceImpl implements ProductSearchService {
         loggingReturnedProduct(product);
         ProductImageDtoCollection imageList = getProductImagesByProductId(productId);
         List<ReviewWithAvgProjection> reviewWithAvg = getProductReviewsByProductId(productId);
-        ImageUrl userPhoto = getImageOfUser(product);
+        // ImageUrl userPhoto = getImageOfUser(product);
         String userNickname = getUserName(product);
-        return ProductDetailResponseDto.of(product, imageList, reviewWithAvg,userPhoto,userNickname);
+        return ProductDetailResponseDto.of(product, imageList, reviewWithAvg,userNickname);
     }
 
     private ProductDto getProductById(Long productId) {
@@ -86,15 +80,15 @@ public class ProductSearchServiceImpl implements ProductSearchService {
         return reviewService.getAllReview(productId);
     }
 
-    private ProfileResponseDto getProfile(Long userId){
+    private ProfileResponse getProfile(Long userId){
         return profileService.getProfile(userId);
     }
 
-    private ImageUrl getImageOfUser(ProductDto product){
-        User user = product.getUser();
-        ProfileResponseDto profile = getProfile(user.getId());
-        return ImageUrl.of(profile.getImage());
-    }
+    // private ImageUrl getImageOfUser(ProductDto product){
+    //     User user = product.getUser();
+    //     ProfileResponse profile = getProfile(user.getId());
+    //     return ImageUrl.of(profile.getImage());
+    // }
 
     private String getUserName(ProductDto product){
 
