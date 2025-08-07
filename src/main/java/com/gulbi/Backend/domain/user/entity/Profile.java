8ce5c09c9c26361966a7c484f28aed8d6fa576f6
woosungking <abuga991008@gmail.com>
2025-08-07 21:58:55 +0900

    refactor: profile 일부 필드 제거

--- src/main/java/com/gulbi/Backend/domain/user/entity/Profile.java
+++ src/main/java/com/gulbi/Backend/domain/user/entity/Profile.java
@@ -1,10 +1,11 @@
 package com.gulbi.Backend.domain.user.entity;
 
 
-import com.gulbi.Backend.domain.user.dto.ProfileRequestDto;
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
+import com.gulbi.Backend.domain.user.dto.ProfileUpdateRequest;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,41 +28,22 @@ public class Profile {
     @JoinColumn(name = "user_id", nullable = false)
     @NotNull
     private User user; // 사용자의 외래키 (FK)
-
-    private String image; // 프로필 이미지 URL
-    private String intro; // 자기소개
     private String phone; // 전화번호
+    @Column(length = 2000)
+
     private String signature; // 전자서명
 
-    @Size(max = 100) // 시도 최대 길이 제한
-    private String sido; // 시도
 
-    @Size(max = 100) // 시군구 최대 길이 제한
-    private String sigungu; // 시군구
-
-    @Size(max = 100) // 읍면동 최대 길이 제한
-    private String bname; // 읍면동
-
-    // ProfileRequestDto와 User를 받아서 Profile 객체를 생성하는 정적 메서드
-    public static Profile fromDto(ProfileRequestDto dto, User user) {
+    public static Profile of(ImageUrl imageUrl, User user) {
         return Profile.builder()
                 .user(user)
-                .image(dto.getImage())
-                .intro(dto.getIntro())
-                .phone(dto.getPhone())
-                .signature(dto.getSignature())
-                .sido(dto.getSido())
-                .sigungu(dto.getSigungu())
-                .bname(dto.getBname())
+                .signature(imageUrl.getImageUrl())
                 .build();
     }
-    public void update(ProfileRequestDto dto) {
-        this.image = dto.getImage();
-        this.intro = dto.getIntro();
+    public void updateText(ProfileUpdateRequest dto) {
         this.phone = dto.getPhone();
-        this.signature = dto.getSignature();
-        this.sido = dto.getSido();
-        this.sigungu = dto.getSigungu();
-        this.bname = dto.getBname();
+    }
+    public void updateSignature(ImageUrl url){
+        this.signature=url.getImageUrl();
     }
 }
