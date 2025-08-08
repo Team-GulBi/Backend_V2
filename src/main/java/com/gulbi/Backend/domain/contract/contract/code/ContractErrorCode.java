1d545fd4dca2176cd09003b8bb99671e3d4b56cd
woosungking <abuga991008@gmail.com>
2025-08-08 17:29:10 +0900

    feat:

---
+++ src/main/java/com/gulbi/Backend/domain/contract/contract/code/ContractErrorCode.java
@@ -0,0 +1,38 @@
+package com.gulbi.Backend.domain.contract.contract.code;
+
+import org.springframework.http.HttpStatus;
+import com.gulbi.Backend.global.response.ResponseApiCode;
+import lombok.Getter;
+
+@Getter
+public enum ContractErrorCode implements ResponseApiCode {
+
+	CONTRACT_NOT_FOUND(HttpStatus.NOT_FOUND, "C100", "계약서를 찾을 수 없습니다."),
+	CONTRACT_DELETE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "C101", "계약서 삭제에 실패했습니다."),
+	CONTRACT_REPO_ERROR (HttpStatus.INTERNAL_SERVER_ERROR, "C102", "계약서 관련작업 중 DB에 문제가 발생 했습니다.");
+
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
+
+	ContractErrorCode(HttpStatus status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+
+	@Override
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	@Override
+	public String getCode() {
+		return code;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
