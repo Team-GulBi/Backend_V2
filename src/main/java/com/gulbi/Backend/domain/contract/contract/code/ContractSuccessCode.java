0026458feb31d622f52784237f33b977def02c17
woosungking <abuga991008@gmail.com>
2025-08-08 16:26:10 +0900

    feat: Contract 응답코드 삽입

--- src/main/java/com/gulbi/Backend/domain/contract/contract/code/ContractSuccessCode.java
+++ src/main/java/com/gulbi/Backend/domain/contract/contract/code/ContractSuccessCode.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public enum ContractSuccessCode implements ResponseApiCode {
 
-    CONTRACT_CREATE_SUCCESS(HttpStatus.OK, "C001", "계약서가 생성되었습니다.");
+    CONTRACT_CREATE_SUCCESS(HttpStatus.OK, "C001", "계약서가 생성되었습니다."),
+    CONTRACT_FOUNDED_SUCCESS(HttpStatus.OK, "C002", "계약서가 조회되었습니다."),
+    CONTRACT_APPROVAL_SUCCESS(HttpStatus.OK, "C003", "계약이 성사 되었습니다."),
+    CONTRACT_UNAPPROVAL_SUCCESS(HttpStatus.OK, "C004", "계약이 반려 되었습니다.(삭제되었습니다)");
 
     private HttpStatus status;
     private String code;
--- src/main/java/com/gulbi/Backend/domain/contract/contract/controller/ContractController.java
+++ src/main/java/com/gulbi/Backend/domain/contract/contract/controller/ContractController.java
@@ -10,6 +10,8 @@ import com.gulbi.Backend.global.response.RestApiResponse;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -38,12 +40,11 @@ public class ContractController {
         RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_CREATE_SUCCESS);
         return ResponseEntity.ok(response);
     }
-    // 예약정보로 계약 조회 ToDo:
+
     @GetMapping("/applications/{applicationId}")
     public ResponseEntity<RestApiResponse> getContractByApplication(@PathVariable("applicationId")Long applicationId){
         ContractResponseDto data = contractService.getContractByApplicationId(applicationId);
-        //ToDo: 응답 코드 알맞게 수정
-        RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_CREATE_SUCCESS,data);
+        RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_FOUNDED_SUCCESS,data);
         return ResponseEntity.ok(response);
     }
 
@@ -54,59 +55,17 @@ public class ContractController {
         @RequestPart MultipartFile finalContract) {
         LenderApprovalCommand command = new LenderApprovalCommand(finalContract,contractId);
         contractService.updateLenderApproval(command);
-        RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_CREATE_SUCCESS);
+        RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_APPROVAL_SUCCESS);
         return ResponseEntity.ok(response);
     }
 
     @DeleteMapping(value = "/{contractId}/lender-approval")
-    public void deleteContract(
+    public ResponseEntity<RestApiResponse> deleteContract(
         @PathVariable("contractId") Long contractId
     ){
         contractService.rejectContraction(contractId);
+        RestApiResponse response = new RestApiResponse(ContractSuccessCode.CONTRACT_UNAPPROVAL_SUCCESS);
+        return ResponseEntity.ok(response);
     }
-
-    // // 특정 계약 조회 (상세 정보)
-    // @GetMapping("/{contractId}")
-    // public ResponseEntity<ContractResponseDto> getContractById(@PathVariable Long contractId) {
-    //     Optional<ContractResponseDto> contract = contractService.getContractById(contractId);
-    //     return contract.map(ResponseEntity::ok)
-    //             .orElseGet(() -> ResponseEntity.notFound().build());
-    // }
-
-   // // 특정 대여인의 계약 조회
-   // @GetMapping("/lender/{lenderId}")
-   // public ResponseEntity<List<ContractSummaryDto>> getContractsByLender(@PathVariable Long lenderId) {
-   //     return ResponseEntity.ok(contractService.getContractsByLender(lenderId));
-   // }
-//
-//    // 특정 차용인의 계약 조회
-//    @GetMapping("/borrower/{borrowerId}")
-//    public ResponseEntity<List<ContractSummaryDto>> getContractsByBorrower(@PathVariable Long borrowerId) {
-//        return ResponseEntity.ok(contractService.getContractsByBorrower(borrowerId));
-//    }
-
-    // // 특정 신청과 관련된 계약 조회
-    // @GetMapping
-    // public ResponseEntity<List<ContractSummaryDto>> getContractsByApplication(@PathVariable Long applicationId) {
-    //     return ResponseEntity.ok(contractService.getContractsByApplication(applicationId));
-    // }
-    //
-
-
-    // // 차용인 승인 상태 변경
-    // @PutMapping("/{contractId}/borrower-approval")
-    // public ResponseEntity<ContractResponseDto> updateBorrowerApproval(
-    //         @PathVariable Long contractId) {
-    //     return ResponseEntity.ok(contractService.updateBorrowerApproval(contractId));
-    // }
-
-    // // 계약 파일 업로드 및 url 업데이트
-    // @PostMapping(value = "/{contractId}/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    // public ResponseEntity<ContractResponseDto> uploadContractFile(
-    //         @PathVariable Long contractId,
-    //         @RequestParam("file") MultipartFile file) throws IOException {
-    //     return ResponseEntity.ok(contractService.uploadContractFile(contractId, file));
-    // }
-
 
 }
