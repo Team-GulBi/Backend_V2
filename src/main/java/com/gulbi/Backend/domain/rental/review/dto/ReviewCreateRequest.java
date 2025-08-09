fdec7e146f874ae528b030ad94ac5f01c712298a
woosungking <abuga991008@gmail.com>
2025-08-09 15:11:29 +0900

    refactor: productId 제외

--- src/main/java/com/gulbi/Backend/domain/rental/review/dto/ReviewCreateRequest.java
+++ src/main/java/com/gulbi/Backend/domain/rental/review/dto/ReviewCreateRequest.java
@@ -11,8 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReviewCreateRequest {
-    @NotNull
-    private Long productId;
+
     @Min(value = 1, message = "Rating must be at least 1")
     @Max(value = 5, message = "Rating must be at most 5")
     private Integer rating;
