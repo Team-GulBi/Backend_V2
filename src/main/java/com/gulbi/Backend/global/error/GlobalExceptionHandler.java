628bfdecd18578bbb4fb3a6b7b425c192183be5f
kimzini <kimjinhee.dev@gmail.com>
2025-08-21 17:52:25 +0900

    fix: 불필요한 import문 삭제

--- src/main/java/com/gulbi/Backend/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/gulbi/Backend/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.gulbi.Backend.global.error;
 
-import com.gulbi.Backend.domain.user.response.ErrorCode;
 import com.gulbi.Backend.global.response.RestApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
