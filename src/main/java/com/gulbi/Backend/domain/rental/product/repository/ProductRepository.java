8c14515ead73ae49b466083833923e4d372bfac7
woosungking <abuga991008@gmail.com>
2025-08-23 20:36:01 +0900

    refactor:

--- src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductRepository.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductRepository.java
@@ -14,47 +14,56 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-    @Query(value = "SELECT p.id AS id, p.main_image AS mainImage, p.title AS title, p.price AS price FROM products p WHERE p.title LIKE CONCAT('%', :query, '%')", nativeQuery = true)
-    List<ProductOverViewResponse> findProductsByTitle(@Param("query")String query);
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // 제목 검색
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse(" +
+        "p.id, p.mainImage, p.title, p.price, p.createdAt) " +
+        "FROM Product p " +
+        "WHERE p.title LIKE CONCAT('%', :query, '%')")
+    List<ProductOverViewResponse> findAllByTitle(@Param("query") String query);
 
-    @Query(value = "SELECT p.id AS id, p.main_image AS mainImage, p.title AS title, p.price AS price " +
-            "FROM Product p " +
-            "WHERE " +
-            "p.tag LIKE CONCAT('%', :query1, '%')" +
-            "AND" +
-            "(:query2 IS NULL OR p.tag LIKE CONCAT('%', :query2, '%'))" +
-            "AND" +
-            "(:query3 IS NULL OR p.tag LIKE CONCAT('%', :query3, '%') ) ", nativeQuery = true)
-    List<ProductOverViewResponse> findProductsByTag(@Param("query1") String tagQuery1, @Param("query2") String tagQuery2, @Param("query3") String tagQuery3);
+    // 태그 검색
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse(" +
+        "p.id, p.mainImage, p.title, p.price, p.createdAt) " +
+        "FROM Product p " +
+        "WHERE (:tag1 IS NULL OR p.tag LIKE CONCAT('%', :tag1, '%')) " +
+        "AND (:tag2 IS NULL OR p.tag LIKE CONCAT('%', :tag2, '%')) " +
+        "AND (:tag3 IS NULL OR p.tag LIKE CONCAT('%', :tag3, '%'))")
+    List<ProductOverViewResponse> findAllByTag(@Param("tag1") String tagQuery1,
+        @Param("tag2") String tagQuery2,
+        @Param("tag3") String tagQuery3);
 
-    @Query(value = "SELECT p.id AS id, p.mainImage AS mainImage, p.title AS title, p.price AS price "+
-                    "FROM Product p"+
-                    " WHERE p.id IN :productIds")
-    List<ProductOverViewResponse> findProductsByIds(@Param("productIds") List<Long> productIds);
+    // ID 리스트 조회
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse(" +
+        "p.id, p.mainImage, p.title, p.price, p.createdAt) " +
+        "FROM Product p " +
+        "WHERE p.id IN :productIds")
+    List<ProductOverViewResponse> findAllOverViewByIdIn(@Param("productIds") List<Long> productIds);
 
-    @Query("SELECT p.id AS id, p.mainImage AS mainImage, p.title AS title, p.price AS price " +
-            "FROM Product p " +
-            "WHERE (:lastCreatedAt IS NULL OR p.createdAt < :lastCreatedAt) " +
-            "ORDER BY p.createdAt DESC")
-    List<ProductOverViewResponse> findAllProductOverviewsByCreatedAtDesc(@Param("lastCreatedAt") LocalDateTime lastCreatedAt, Pageable pageable);
+    // 생성일 기준 조회
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse(" +
+        "p.id, p.mainImage, p.title, p.price, p.createdAt) " +
+        "FROM Product p " +
+        "WHERE (:lastCreatedAt IS NULL OR p.createdAt < :lastCreatedAt) " +
+        "ORDER BY p.createdAt DESC")
+    List<ProductOverViewResponse> findAllOverviewByCreatedAtDesc(@Param("lastCreatedAt") LocalDateTime lastCreatedAt,
+        Pageable pageable);
 
-
-    @Query(value = "SELECT p FROM Product p WHERE " +
-            "(:bCategoryId IS NULL OR :bCategoryId = 0 OR p.bCategory.id = :bCategoryId) AND " +
-            "(:mCategoryId IS NULL OR :mCategoryId = 0 OR p.mCategory.id = :mCategoryId) AND " +
-            "(:sCategoryId IS NULL OR :sCategoryId = 0 OR p.sCategory.id = :sCategoryId) AND " +
-            "(:lastCreatedAt IS NULL OR p.createdAt < :lastCreatedAt)")
-    List<ProductOverViewResponse> findAllProductByCategoryIds(
-            @Param("bCategoryId") Long bCategoryId,
-            @Param("mCategoryId") Long mCategoryId,
-            @Param("sCategoryId") Long sCategoryId,
-            @Param("lastCreatedAt") LocalDateTime lastCreatedAt,
-            Pageable pageable);
-
+    // 카테고리 조회
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse(" +
+        "p.id, p.mainImage, p.title, p.price, p.createdAt) " +
+        "FROM Product p " +
+        "WHERE (:bCategoryId IS NULL OR :bCategoryId = 0 OR p.bCategory.id = :bCategoryId) " +
+        "AND (:mCategoryId IS NULL OR :mCategoryId = 0 OR p.mCategory.id = :mCategoryId) " +
+        "AND (:sCategoryId IS NULL OR :sCategoryId = 0 OR p.sCategory.id = :sCategoryId) " +
+        "AND (:lastCreatedAt IS NULL OR p.createdAt < :lastCreatedAt)")
+    List<ProductOverViewResponse> findAllByCategoryIds(@Param("bCategoryId") Long bCategoryId,
+        @Param("mCategoryId") Long mCategoryId,
+        @Param("sCategoryId") Long sCategoryId,
+        @Param("lastCreatedAt") LocalDateTime lastCreatedAt,
+        Pageable pageable);
 
     Optional<Product> findProductById(@Param("id") Long id);
 
@@ -66,10 +75,8 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
         "WHERE p.id = :id")
     Optional<Product> findByIdWithAll(@Param("id") Long id);
 
-
-
     @Transactional
     @Modifying
-    @Query("DELETE FROM Product p WHERE p.id=:productId")
+    @Query("DELETE FROM Product p WHERE p.id = :productId")
     void deleteAllById(@Param("productId") Long productId);
 }
