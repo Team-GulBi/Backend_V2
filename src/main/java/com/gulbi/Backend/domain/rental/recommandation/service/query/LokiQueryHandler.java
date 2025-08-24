2e54bbd13a82442c6bc952a052aeb47af8d8fe4b
woosungking <abuga991008@gmail.com>
2025-08-24 23:52:41 +0900

    refactor: ExtractedRecommendation => PriorityCategoriesMap

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/LokiQueryHandler.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/LokiQueryHandler.java
@@ -1,48 +1,55 @@
 package com.gulbi.Backend.domain.rental.recommandation.service.query;
 
+import static com.gulbi.Backend.domain.rental.recommandation.service.query.JsonPathQuery.*;
+
 import com.gulbi.Backend.domain.rental.recommandation.code.JsonPathErrorCode;
+import com.gulbi.Backend.domain.rental.recommandation.dto.CategoryPair;
+import com.gulbi.Backend.domain.rental.recommandation.dto.PriorityCategoriesMap;
 import com.gulbi.Backend.domain.rental.recommandation.exception.JsonPathException;
-import com.gulbi.Backend.domain.rental.recommandation.vo.ExtractedProductIds;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PopularProductIds;
 import com.gulbi.Backend.domain.rental.recommandation.vo.ExtractedRecommendation;
 import com.gulbi.Backend.global.error.ExceptionMetaData;
 import com.jayway.jsonpath.JsonPath;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class LokiQueryHandler implements QueryHandler{
     private final String className = this.getClass().getName();
-    private static final String EXTRACT_BIG_CATEGORY_IDS ="$.data.result[*].metric.bCategoryId";
-    private static final String EXTRACT_MIDDLE_CATEGORY_IDS ="$.data.result[*].metric.mCategoryId";
-    private static final String EXTRACT_PRIORITY ="$.data.result[*].value[1]";
-    @Override
-    public ExtractedProductIds getListOfProductIds(String queryResult) {
-        return ExtractedProductIds.of(JsonPath.read(queryResult, "$.data.result[*].metric.productId"));
-    }
 
     @Override
-    public ExtractedRecommendation getMapOfRecommandation(String queryResult) {
-        // 실패작들
-        //        System.out.println(Optional.ofNullable(JsonPath.read(queryResult, "$.data.result[*].value[*].values")));
-        //        System.out.println(Optional.ofNullable(JsonPath.read(queryResult, "$.data.result[*].metric[*].bCategoryId")));
-//        System.out.println(queryResult);
-//        System.out.println(Optional.ofNullable(JsonPath.read(queryResult, "$.data.result[*].metric.bCategoryId")));
-//        System.out.println(Optional.ofNullable(JsonPath.read(queryResult, "$.data.result[*].metric.mCategoryId")));
-//        System.out.println(Optional.ofNullable(JsonPath.read(queryResult, "$.data.result[*].value[1]")));
-        try {
-            List<String> bCategoryList = JsonPath.read(queryResult, EXTRACT_BIG_CATEGORY_IDS);
-            List<String> mCategoryList = JsonPath.read(queryResult, EXTRACT_MIDDLE_CATEGORY_IDS);
-            List<String> priorityList = JsonPath.read(queryResult, EXTRACT_PRIORITY);
-            return ExtractedRecommendation.of(bCategoryList, mCategoryList, priorityList);
-        }catch(Exception e){
-            ExceptionMetaData exceptionMetaData = new ExceptionMetaData.Builder()
-                    .responseApiCode(JsonPathErrorCode.JSONPATH_CANT_PARSE_QUERY)
-                    .className(className)
-                    .args(queryResult)
-                    .stackTrace(e).build();
-            throw new JsonPathException.jsonPathCantParseQueryException(exceptionMetaData);
+    public PopularProductIds extractProductIdsFromQueryResult(String queryResult) {
+        // 추출된 JSON 문자열에서 ProductIds만 추출
+        List<String> productIds = JsonPath.read(queryResult, EXTRACT_PRODUCT_IDS.getQuery());
+        return PopularProductIds.fromStrings(productIds);
+    }
+
+
+    @Override
+    public PriorityCategoriesMap getPriorityCategoriesMap(String queryResult) {
+        List<String> countList = JsonPath.read(queryResult, TESt_QUERY.getQuery());
+        int count = countList.size() - 1; // 마지막 인덱스부터 시작
+        PriorityCategoriesMap map = new PriorityCategoriesMap();
+
+        for (; count >= 0; count--) { // 0까지 감소
+            String testQuery = String.format(JsonPathQuery.TEST_QUERY_2.getQuery(), count);
+            String testQuery2 = String.format(JsonPathQuery.TEST_QUERY_3.getQuery(), count);
+            String testQuery3 = String.format(JsonPathQuery.TEST_QUERY_4.getQuery(), count,1);
+
+            String bigCategoryId = JsonPath.read(queryResult,testQuery);
+            String midCategoryId = JsonPath.read(queryResult,testQuery2);
+            String priority = JsonPath.read(queryResult,testQuery3);
+
+            CategoryPair categories = new CategoryPair(Long.parseLong(bigCategoryId),Long.parseLong(midCategoryId));
+            map.put(priority,categories);
         }
+        return map;
 
     }
 
