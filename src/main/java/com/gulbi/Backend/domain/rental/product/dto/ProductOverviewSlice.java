d9160c42c4154336745b43c140a79643fb000c08
woosungking <abuga991008@gmail.com>
2025-08-25 00:04:51 +0900

    refactor: nickname 반환

--- src/main/java/com/gulbi/Backend/domain/rental/product/dto/ProductOverviewSlice.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/dto/ProductOverviewSlice.java
@@ -1,17 +1,23 @@
 package com.gulbi.Backend.domain.rental.product.dto;
 
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductOverviewSlice {
+	private String nickname;
+	private boolean hasNext;
+	private List<ProductOverViewResponse> products;
 
-	private final boolean hasNext;
-	private final List<ProductOverViewResponse> products;
-
-	public ProductOverviewSlice(boolean hasNext, List<ProductOverViewResponse> products) {
-		this.hasNext = hasNext;
-		this.products = products;
-	}
-
+public ProductOverviewSlice(boolean hasNext, List<ProductOverViewResponse> products){
+	this.hasNext = hasNext;
+	this.products = products;
+	this.nickname = null;
+}
 }
