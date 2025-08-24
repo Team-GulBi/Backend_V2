a17d214d66c0e5edaa5b736d1cc9432b0cae2bb3
woosungking <abuga991008@gmail.com>
2025-08-24 23:50:48 +0900

    feat: getMapOfRecommandatio =>getPriorityCategoriesMap

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/QueryHandler.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/QueryHandler.java
@@ -1,10 +1,11 @@
 package com.gulbi.Backend.domain.rental.recommandation.service.query;
 
-import com.gulbi.Backend.domain.rental.recommandation.vo.ExtractedProductIds;
+import com.gulbi.Backend.domain.rental.recommandation.dto.PriorityCategoriesMap;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PopularProductIds;
 import com.gulbi.Backend.domain.rental.recommandation.vo.ExtractedRecommendation;
 
 public interface QueryHandler {
 
-    ExtractedProductIds getListOfProductIds(String queryResult);
-    ExtractedRecommendation getMapOfRecommandation(String queryResult);
+    PopularProductIds extractProductIdsFromQueryResult(String queryResult);
+    PriorityCategoriesMap getPriorityCategoriesMap(String queryResult);
 }
