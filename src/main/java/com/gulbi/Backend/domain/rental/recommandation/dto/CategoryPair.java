8b7ede516869a7d296df13c572210f16698ec23e
woosungking <abuga991008@gmail.com>
2025-08-25 17:33:07 +0900

    refactor: 삭제예정

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/dto/CategoryPair.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/dto/CategoryPair.java
@@ -1,13 +1,17 @@
-package com.gulbi.Backend.domain.rental.recommandation.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-@Builder
-public class CategoryPair {
-	private final Long bigCategoryId;
-	private final Long midCategoryId;
-}
+// package com.gulbi.Backend.domain.rental.recommandation.dto;
+//
+// import java.util.UUID;
+//
+// import lombok.Builder;
+// import lombok.Getter;
+// import lombok.RequiredArgsConstructor;
+//
+// @Getter
+// @RequiredArgsConstructor
+// @Builder
+// public class CategoryPair {
+// 	private final Long bigCategoryId;
+// 	private final Long midCategoryId;
+// 	private final Long count;
+// 	private final UUID uuid;
+// }
