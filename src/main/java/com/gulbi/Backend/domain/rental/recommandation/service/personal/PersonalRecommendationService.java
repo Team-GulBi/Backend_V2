55420143f15a4112c48f1b1afef2e9df3b3f18e3
woosungking <abuga991008@gmail.com>
2025-08-25 17:33:12 +0900

    refactor:

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/personal/PersonalRecommendationService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/personal/PersonalRecommendationService.java
@@ -1,18 +1,10 @@
 package com.gulbi.Backend.domain.rental.recommandation.service.personal;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.gulbi.Backend.domain.rental.product.service.product.logging.LokiLoggingHandler;
-import com.gulbi.Backend.domain.rental.recommandation.dto.PriorityCategoriesMap;
-import com.gulbi.Backend.domain.rental.recommandation.service.personal.strategy.RecommendationStrategyProviderService;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PriorityCategoriesMap;
 import com.gulbi.Backend.domain.rental.recommandation.service.query.LogQueryService;
 import com.gulbi.Backend.domain.rental.recommandation.service.query.QueryHandler;
-import com.gulbi.Backend.domain.rental.recommandation.vo.ExtractedRecommendation;
-import com.gulbi.Backend.domain.rental.recommandation.dto.PersonalRecommendationRequestDto;
 import com.gulbi.Backend.domain.rental.recommandation.dto.PersonalRecommendationResponseDto;
-import com.gulbi.Backend.global.CursorPageable;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PriorityCategoriesQueue;
 import com.gulbi.Backend.global.PersonalCursorPageable;
 
 import org.springframework.stereotype.Service;
@@ -31,10 +23,13 @@ public class PersonalRecommendationService implements PersonalProductProvider {
     public PersonalRecommendationResponseDto getPersonalizedRecommendationProducts(PersonalCursorPageable request) {
         //커서가 없으면 최초 요청임. 3개 => 7:3:2 비율로
         if(request.isEmptyCursor()){
+			// 요청을 보낸 유저의 관심사 카테고리 쌍을 3개를 뽑는 쿼리(추후 더 확장이 가능 함)
             String result = logQueryService.getQueryOfMostViewedCategoriesByUser();
-            PriorityCategoriesMap categoriesMap = queryHandler.getPriorityCategoriesMap(result);
-            // 커서가 없으면 최초 요청이니 우선순위 대로 7 3 2 ??..
-            //
+			// 해당 쌍을 가공 후 맵으로 만듬.
+			// 맵이지만 pop을 할때 queue처럼 써야함.
+            PriorityCategoriesQueue priorityCategoriesQueue = queryHandler.getPriorityCategoriesMap(result);
+
+			//pageable의 size체크 priority size 체크 6 3 1 비율로,
 
 
         }
