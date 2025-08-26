50e222f1f116dd189ccbf86d3e562c2daab30d9d
woosungking <abuga991008@gmail.com>
2025-08-26 15:22:20 +0900

    feat: booleanExpression 추가(카테고리)

--- src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductCustomRepositoryImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductCustomRepositoryImpl.java
@@ -50,11 +50,15 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
 				product.createdAt.as("createdAt")
 			))
 			.from(product)
-			.where( //제목에 부합, 특정 유저 아이디, 대신 null이면 동작 안함
-				cursorCondition(product, lastId, lastTime),
-				titleContains(condition.getTitle()),
-				userEq(condition.getUser())
+			.where(
+				cursorCondition(product, lastId, lastTime), //모든 상품 조회
+				titleContains(condition.getTitle()), // 제목으로 조회시 조건
+				userEq(condition.getUser()), // 해당 유저의 상품인지 조건
+				bCategoryEq(condition.getBCategoryId()),
+				mCategoryEq(condition.getMCategoryId()),
+				sCategoryEq(condition.getSCategoryId())
 			)
+
 			.orderBy(toOrderSpecifiers(pageable.getSort()))
 			.limit(limit + 1)
 			.fetch();
@@ -95,4 +99,16 @@ public class ProductCustomRepositoryImpl implements ProductCustomRepository {
 			})
 			.toArray(OrderSpecifier[]::new);
 	}
+	private BooleanExpression bCategoryEq(Long bCategoryId) {
+		return (bCategoryId == null) ? null : QProduct.product.bCategory.id.eq(bCategoryId);
+	}
+
+	private BooleanExpression mCategoryEq(Long mCategoryId) {
+		return (mCategoryId == null) ? null : QProduct.product.mCategory.id.eq(mCategoryId);
+	}
+
+	private BooleanExpression sCategoryEq(Long sCategoryId) {
+		return (sCategoryId == null) ? null : QProduct.product.sCategory.id.eq(sCategoryId);
+	}
+
 }
