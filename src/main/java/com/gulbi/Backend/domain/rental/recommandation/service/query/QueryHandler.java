6d946976e84399a10c784f6fccd8e132879549b3
woosungking <abuga991008@gmail.com>
2025-08-26 15:24:55 +0900

    refactor: 불필요한 의존성 제거

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/QueryHandler.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/query/QueryHandler.java
@@ -1,6 +1,5 @@
 package com.gulbi.Backend.domain.rental.recommandation.service.query;
 
-import com.gulbi.Backend.domain.rental.recommandation.vo.PriorityCategoriesMap;
 import com.gulbi.Backend.domain.rental.recommandation.vo.PopularProductIds;
 import com.gulbi.Backend.domain.rental.recommandation.vo.PriorityCategoriesQueue;
 
