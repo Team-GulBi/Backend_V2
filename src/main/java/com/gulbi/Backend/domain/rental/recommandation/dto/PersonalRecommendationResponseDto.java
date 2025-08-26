532ee9c5114f5234911380a5b4a3a2255ef4081f
woosungking <abuga991008@gmail.com>
2025-08-26 15:05:52 +0900

    refactor: 나는 실패작이래

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/dto/PersonalRecommendationResponseDto.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/dto/PersonalRecommendationResponseDto.java
@@ -1,15 +1,15 @@
-package com.gulbi.Backend.domain.rental.recommandation.dto;
-
-import com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse;
-import lombok.Getter;
-
-import java.util.List;
-@Getter
-public class PersonalRecommendationResponseDto {
-    private List<List<ProductOverViewResponse>> products;
-
-    public PersonalRecommendationResponseDto(List<List<ProductOverViewResponse>> products) {
-        this.products = products;
-    }
-    public PersonalRecommendationResponseDto(){}
-}
+// package com.gulbi.Backend.domain.rental.recommandation.dto;
+//
+// import com.gulbi.Backend.domain.rental.product.dto.ProductOverViewResponse;
+// import lombok.Getter;
+//
+// import java.util.List;
+// @Getter
+// public class PersonalRecommendationResponseDto {
+//     private List<List<ProductOverViewResponse>> products;
+//
+//     public PersonalRecommendationResponseDto(List<List<ProductOverViewResponse>> products) {
+//         this.products = products;
+//     }
+//     public PersonalRecommendationResponseDto(){}
+// }
